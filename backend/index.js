9e1fa2d6aba9a63bef633dc19055e4bde32bcf63
Sahil Chouhan <chouhangolu604@gmail.com>
2026-05-10 12:40:45 +0530

    url changed again

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.port
 connectDB()
 // app.use(cors())
 app.use(cors({
-  origin: "https://chat-application-111.netlify.app",
+  origin: "http://localhost:5173",
   credentials: true
 }));
 app.use(urlencoded({extended: true}))
