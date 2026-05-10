a09ab5ff15286622d7ea52ab52533402f968cfcc
Sahil Chouhan <chouhangolu604@gmail.com>
2026-05-10 12:57:52 +0530

    -

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault()
     try {
-      const res = await API.post(`http://localhost:8080/api/v1/user/login`, user, { withCredentials: true })
+      const res = await API.post(`/api/v1/user/login`, user, { withCredentials: true })
       if (res.data.success) {
         localStorage.setItem(
           "user",
